27eac06de9be09509185f2d59c3711425bfae1c8
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2022-11-16 14:16:19 +0100

    ad dmissing packages

--- scripts/meetup_auth.R
+++ scripts/meetup_auth.R
@@ -1,21 +1,25 @@
 cat("Session is non-interactive. Starting meetup authentication process.\n")
+library(cyphr)
+library(sodium)
+library(meetupr)
+library(here)
 
 temptoken <- tempfile(fileext = ".rds")
 encr_path <- "scripts/secret_encr.rds"
 
-key <- cyphr::key_sodium(sodium::hex2bin(Sys.getenv("MEETUPR_PWD")))
+key <- key_sodium(hex2bin(Sys.getenv("MEETUPR_PWD")))
 
-cyphr::decrypt_file(
-  here::here(encr_path),
+decrypt_file(
+  here(encr_path),
   key = key,
   dest = temptoken
 )
 
 token <- readRDS(temptoken)[[1]]
-token <- meetupr::meetup_auth(
+token <- meetup_auth(
   token = temptoken,
   cache = FALSE
 ) 
 
 cat("\t authenticating...\n\n")
-k <- meetupr::meetup_auth(token = temptoken)
+k <- meetup_auth(token = temptoken)
