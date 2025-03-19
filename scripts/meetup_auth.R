69e50a366cb4dfbc82999c344b44ee709b4457af
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-03-19 12:51:12 +0100

    update action and renv

--- scripts/meetup_auth.R
+++ scripts/meetup_auth.R
@@ -7,7 +7,9 @@ library(here)
 temptoken <- tempfile(fileext = ".rds")
 encr_path <- "scripts/secret_encr.rds"
 
-key <- key_sodium(hex2bin(Sys.getenv("MEETUPR_PWD")))
+key <- key_sodium(
+  hex2bin(Sys.getenv("MEETUPR_PWD"))
+)
 
 decrypt_file(
   here(encr_path),
@@ -19,7 +21,7 @@ token <- readRDS(temptoken)[[1]]
 token <- meetup_auth(
   token = temptoken,
   cache = FALSE
-) 
+)
 
 cat("\t authenticating...\n\n")
 k <- meetup_auth(token = temptoken)
--- scripts/utils.R
+++ scripts/utils.R
@@ -1,32 +1,35 @@
-# If not running interactively, 
+# If not running interactively,
 # get token decrypted from env var
-if(!interactive()){
+if (!interactive()) {
   source(here::here("scripts/meetup_auth.R"))
 }
 
 # Define helper functions ----
 # change empty to NA
-change_empty <- function(x) ifelse(x == "", NA, x)
+change_empty <- function(x) {
+  ifelse(x == "", NA, x)
+}
 
+
+# default value if given is NA
+`%||%` <- function(a, b) {
+  ifelse(!is.na(a), a, b)
+}
 
 # Clean-up chapter name and create link
-get_chapter <- function(x){
+get_chapter <- function(x) {
   y <- sapply(x, function(x) strsplit(x, "/")[[1]][4])
   unname(y)
 }
 
-
 # Force UTC timezone
-force_utc <- function(datetime, tz){
+force_utc <- function(datetime, tz) {
   x <- as_datetime(datetime, tz = tz)
   with_tz(x, "UTC")
 }
 
-# default value if given is NA
-`%||%` <- function(a, b) ifelse(!is.na(a), a, b)
-
-
-na_col_rm <- function(x){
+# Remove columns with only NA's
+na_col_rm <- function(x) {
   indx <- apply(x, 2, function(y) all(is.na(y)))
   as_tibble(x[, !indx])
 }
